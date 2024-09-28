c8f0a83628a53464b7a9640e3aca06d158a8d361
dantvi <daniel.tving@gmail.com>
2024-09-28 12:49:04 +0200

    authenticating with firebase

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
+import SignIn from "./routes/sign-in/sign-in.component";
 
 const Shop = () => {
   return (
@@ -11,7 +12,7 @@ const Shop = () => {
       </div>
     </div>
   );
-}
+};
 
 const App = () => {
   return (
@@ -19,6 +20,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
+        <Route path="signin" element={<SignIn />} />
       </Route>
     </Routes>
   );
