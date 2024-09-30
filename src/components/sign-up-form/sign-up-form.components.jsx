9eed91f7cf5bcfca07cfbc888dcb1bc10b1e3c6b
dantvi <daniel.tving@gmail.com>
2024-09-30 19:55:04 +0200

    sign up form fields

---
+++ src/components/sign-up-form/sign-up-form.components.jsx
@@ -0,0 +1,25 @@
+
+const SignUpForm = () => {
+    return (
+        <div>
+            <h1>Sign up with your email and password</h1>
+            <form onSubmit={() => {}}>
+                <label htmlFor="">Display Name</label>
+                <input type="text" required/>
+
+                <label htmlFor="">Email</label>
+                <input type="email" required/>
+
+                <label htmlFor="">Password</label>
+                <input type="password" required/>
+
+                <label htmlFor="">Confirm Password</label>
+                <input type="password" required/>
+
+                <button type="submit">Sign Up</button>
+            </form>
+        </div>
+    );
+}
+
+export default SignUpForm;
