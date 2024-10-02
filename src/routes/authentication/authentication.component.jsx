e2a82af220f61c169fb3b5668278e277beb1c773
dantvi <daniel.tving@gmail.com>
2024-10-02 13:08:27 +0200

    sign in form

---
+++ src/routes/authentication/authentication.component.jsx
@@ -0,0 +1,19 @@
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+} from '../../utils/firebase/firebase.utils';
+
+import SignUpForm from '../../components/sign-up-form/sign-up-form.components';
+import SignInForm from '../../components/sign-in-form/sign-in-form.components';
+
+const Authentication = () => {
+  return (
+    <div>
+      <h1>Sign In Page</h1>
+      <SignInForm />
+      <SignUpForm />
+    </div>
+  );
+};
+
+export default Authentication;
